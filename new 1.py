d7669065b931e4ce83d94fa9ab3dbea0bdd16146
nardassimp <141756442+nardassimp@users.noreply.github.com>
2023-08-09 01:45:21 +0800

    Add files via upload

---
+++ new 1.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    float Boy=2.1;
+    double Girl=2.1;
+    
+ //When Boy propose to Girl
+ 
+ if(Boy==Girl){
+    printf("Cute mo")
+ }
+ else{
+    printf("Ganda mo")
+ }
+ return 0;
+ }
+ 
